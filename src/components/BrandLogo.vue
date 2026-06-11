5bbc614d5016016783a990c65c5fac526fd87a12
frank <plh0410@126.com>
2026-06-12 00:52:27 +0800

    Update restaurant brand title

--- src/components/BrandLogo.vue
+++ src/components/BrandLogo.vue
@@ -1,6 +1,6 @@
 <template>
-  <a class="brand" href="#home" aria-label="叙味首页">
-    <span class="brand-mark">叙</span>
-    <span class="brand-name">叙味 <small>SEASON &amp; STORY</small></span>
+  <a class="brand" href="#home" aria-label="时序首页">
+    <span class="brand-mark">时</span>
+    <span class="brand-name">时序 <small>SEASONAL CHINESE DINING</small></span>
   </a>
 </template>
